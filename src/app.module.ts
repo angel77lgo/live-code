f386b4256bca1b0d283c5c0676d83ea52c960b8f
angel77lgo <angel.de.oz.97@gmail.com>
2022-05-30 12:40:32 -0500

    add modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { OrderModule } from './order/order.module';
+import { DinerModule } from './diner/diner.module';
+import { UserModule } from './user/user.module';
+import { ProductModule } from './product/product.module';
+import { ReportModule } from './report/report.module';
 
 @Module({
-  imports: [],
+  imports: [OrderModule, DinerModule, UserModule, ProductModule, ReportModule],
   controllers: [AppController],
   providers: [AppService],
 })
